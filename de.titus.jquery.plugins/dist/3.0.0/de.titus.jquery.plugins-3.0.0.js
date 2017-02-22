e340d7967f079cd6dcea92a2b20891f34d1ea733
Frank Schüler <SchuelerFrank@gmx.de>
2017-02-22 22:33:01 +0100

    update

--- de.titus.jquery.plugins/dist/3.0.0/de.titus.jquery.plugins-3.0.0.js
+++ de.titus.jquery.plugins/dist/3.0.0/de.titus.jquery.plugins-3.0.0.js
@@ -2399,30 +2399,6 @@ de.titus.core.Namespace.create("de.titus.jstl.TaskRegistry", function() {
 		de.titus.jstl.Processor = Processor;
 	});
 })(jQuery, de.titus.core.SpecialFunctions);
-(function(PHASE) {
-	de.titus.core.Namespace.create("de.titus.jstl.Setup", function() {
-		de.titus.jstl.Setup = function() {
-		};
-		
-//		de.titus.jstl.TaskRegistry.append("preprocessor", PHASE.INIT, undefined, de.titus.jstl.functions.Preprocessor.TASK);
-//		de.titus.jstl.TaskRegistry.append("if", PHASE.CONDITION, "[jstl-if]", de.titus.jstl.functions.If.TASK);
-//		de.titus.jstl.TaskRegistry.append("data", PHASE.CONTEXT, "[jstl-data]", de.titus.jstl.functions.Data.TASK);
-//		
-//		de.titus.jstl.TaskRegistry.append("include", PHASE.MANIPULATION, "[jstl-include]", de.titus.jstl.functions.Include.TASK);
-//		de.titus.jstl.TaskRegistry.append("choose", PHASE.MANIPULATION, "[jstl-choose]", de.titus.jstl.functions.Choose.TASK);
-//		de.titus.jstl.TaskRegistry.append("foreach", PHASE.MANIPULATION, "[jstl-foreach]", de.titus.jstl.functions.Foreach.TASK);
-//		
-//		de.titus.jstl.TaskRegistry.append("databind", PHASE.BINDING, "[jstl-databind]", de.titus.jstl.functions.Databind.TASK);
-//		de.titus.jstl.TaskRegistry.append("eventbind", PHASE.BINDING, "[jstl-eventbind]", de.titus.jstl.functions.Eventbind.TASK);
-//		
-//		de.titus.jstl.TaskRegistry.append("add-attribute", PHASE.CONTENT, "[jstl-add-attribute]", de.titus.jstl.functions.AddAttribute.TASK);
-//		de.titus.jstl.TaskRegistry.append("text", PHASE.CONTENT, undefined, de.titus.jstl.functions.Text.TASK);
-//		de.titus.jstl.TaskRegistry.append("attribute", PHASE.CONTENT, undefined, de.titus.jstl.functions.Attribute.TASK);
-//		
-//		de.titus.jstl.TaskRegistry.append("children", PHASE.CHILDREN, undefined, de.titus.jstl.functions.Children.TASK);
-		
-	});
-})(de.titus.jstl.Constants.PHASE)
 de.titus.core.Namespace.create("de.titus.jstl.javascript.polyfills", function() {
 //https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/from#Polyfill	
 
