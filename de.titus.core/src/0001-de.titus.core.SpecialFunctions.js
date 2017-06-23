cb5a77085f03e7a0a740f5c6eadfd44dfd41a324
Frank Schüler <SchuelerFrank@gmx.de>
2017-06-23 10:46:16 +0200

    update

--- de.titus.core/src/0001-de.titus.core.SpecialFunctions.js
+++ de.titus.core/src/0001-de.titus.core.SpecialFunctions.js
@@ -1,38 +1,42 @@
 de.titus.core.Namespace.create("de.titus.core.SpecialFunctions", function() {
-	
+
 	var SpecialFunctions = de.titus.core.SpecialFunctions = {
-		 STATEMENTS : {},
-		    doEval : function(aStatement, aContext, aCallback) {
-			    if (aCallback)
-				    SpecialFunctions.doEvalWithContext(aStatement, (aContext || {}), undefined, aCallback);
+	    DEVMODE : location.search ? (/.*devmode=true.*/ig).test(location.search) : false,
+	    STATEMENTS : {},
+	    doEval : function(aStatement, aContext, aCallback) {
+		    if (aCallback)
+			    SpecialFunctions.doEvalWithContext(aStatement, (aContext || {}), undefined, aCallback);
+		    else {
+			    if (typeof aStatement !== "string")
+				    return aStatement;
 			    else {
-				    if (typeof aStatement !== "string")
-					    return aStatement;
-				    else {
-					    var runContext = aContext || {};
-					    if(runContext["$$$$___STATEMENT___VALUE___$$$___TEXT___$$$$"] !== undefined)
-					    	return undefined;
-					    
-					    var $$$$___STATEMENT___VALUE___$$$___TEXT___$$$$ = aStatement;				    
-					    with (runContext) {
-						    try {
-							    return eval("(function(){ return " + $$$$___STATEMENT___VALUE___$$$___TEXT___$$$$ + ";})()");
-						    } catch (e) {
-							    if (!console)
-								    return;
-							    else if (console.error)
-								    console.error("de.titus.core.SpecialFunctions.doEval ***Error*** expression: " + aStatement + ": ", e);
-							    else if (console.log)
-								    console.log("de.titus.core.SpecialFunctions.doEval ***Error*** expression: " + aStatement + ": ", e);
-							    return undefined;
-						    }
+				    var runContext = aContext || {};
+				    if (runContext["$$$$___STATEMENT___VALUE___$$$___TEXT___$$$$"] !== undefined)
+					    return undefined;
+
+				    var $$$$___STATEMENT___VALUE___$$$___TEXT___$$$$ = aStatement;
+				    with (runContext) {
+					    try {
+						    return eval("(function(){ return " + $$$$___STATEMENT___VALUE___$$$___TEXT___$$$$ + ";})()");
+					    } catch (e) {
+					    	if(SpecialFunctions.DEVMODE){					    		
+					    		console.log("----------------------\n",
+					    		"doEval()\n",	
+					    		"statement: \"" + aStatement + "\"\n",
+					    		"context: \"" + (aContext ? JSON.stringify(aContext) : "{}") + "\"\n",
+					    		"callback: \"" + aCallback + "\"\n",
+					    		"error: ", e, "\n",
+					    		"----------------------");
+					    	}
+					    	throw e;
 					    }
 				    }
-				    
-				    return undefined;
 			    }
-		    },
-	    
+
+			    return undefined;
+		    }
+	    },
+
 	    /**
 		 * 
 		 * @param aStatement
@@ -47,7 +51,7 @@ de.titus.core.Namespace.create("de.titus.core.SpecialFunctions", function() {
 				    var result = SpecialFunctions.doEvalWithContext(aStatement, aContext, aDefault, undefined);
 				    aCallback(result, aContext);
 			    }, 1);
-			    
+
 		    } else
 			    try {
 				    var result = SpecialFunctions.doEval(aStatement, aContext);
@@ -59,5 +63,5 @@ de.titus.core.Namespace.create("de.titus.core.SpecialFunctions", function() {
 			    }
 	    }
 	};
-	
+
 });
--- de.titus.jstl-typeahead/dist/1.0.1/de.titus.jstl-typeahead-1.0.1.js
+++ de.titus.jstl-typeahead/dist/1.0.1/de.titus.jstl-typeahead-1.0.1.js
@@ -1,3 +1,15 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 Frank Schüler
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 (function($, ExpressionResolver) {
 	"use strict";
 	de.titus.core.Namespace.create("de.titus.jquery.Typeahead", function() {
@@ -170,12 +182,12 @@
 			if (this.data.displayMarker) {
 				var regex = "";
 				for (var i = 0; i < aValues.length; i++) {
-					if(i == 0)
+					if(i > 0)
 						regex +="|";
 					regex +=aValues[i];
 				}
 				regex = "[^<](" + regex +")[^>]";
-				
+				console.log(regex);
 				return  aDisplay.replace(new RegExp(regex, "i"), function(aMatch) {
 					return "<b>" + aMatch + "</b>";
 				});
