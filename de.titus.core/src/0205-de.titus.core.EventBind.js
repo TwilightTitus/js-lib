84076c3e99ab02cd449942a4cfbd141169f5746b
Frank Schüler <SchuelerFrank@gmx.de>
2017-09-20 20:11:08 +0200

    update

--- de.titus.core/src/0205-de.titus.core.EventBind.js
+++ de.titus.core/src/0205-de.titus.core.EventBind.js
@@ -3,73 +3,87 @@
 		"use strict";
 		var EventBind = de.titus.core.EventBind = function(anElement, aContext) {
 			if (anElement.data(de.titus.core.EventBind.STATE.FINISHED) == undefined) {
-				
-				var eventType = anElement.attr("event-type");
-				if (eventType == undefined || eventType.trim().length == 0) {
+				var result = {
+				    preventDefault : (typeof anElement.attr("event-prevent-default") !== "undefined"),
+				    stopPropagation : (typeof anElement.attr("event-stop-propagation") !== "undefined")
+				};
+				result.eventType = anElement.attr("event-type");
+				if (result.eventType == undefined || result.eventType.trim().length == 0) {
 					anElement.data(de.titus.core.EventBind.STATE.FINISHED, de.titus.core.EventBind.FINISHEDSTATE.FAIL);
-					return this;
+					return anElement;
 				}
-				
-				var action = anElement.attr("event-action");
-				if (action == undefined || action.trim().length == 0) {
+
+				result.action = anElement.attr("event-action");
+				if (result.action == undefined || result.action.trim().length == 0) {
 					anElement.data(de.titus.core.EventBind.STATE.FINISHED, de.titus.core.EventBind.FINISHEDSTATE.FAIL);
-					return this;
+					return anElement;
 				}
-				
+				if (EventBind.FUNCTIONS[result.action]) {
+					result.defaultAction = true;
+					result.action = EventBind.FUNCTIONS[result.action];
+				}
+
 				var data = undefined;
-				var eventData = anElement.attr("event-data");
-				if (eventData != undefined && eventData.trim().length > 0){
-					data = de.titus.core.EventBind.EXPRESSIONRESOLVER.resolveExpression(eventData, aContext , {});
-				}
-				else if(aContext != undefined){
+				result.eventData = anElement.attr("event-data");
+				if (result.eventData != undefined && result.eventData.trim().length > 0) {
+					data = de.titus.core.EventBind.EXPRESSIONRESOLVER.resolveExpression(eventData, aContext, {});
+				} else if (aContext != undefined) {
 					data = $().extend({}, aContext);
-				}
-				else {
+				} else {
 					data = {};
 				}
-				
+
 				anElement.on(eventType, null, data, de.titus.core.EventBind.$$__execute__$$);
+				anElement.data("de.titus.core.EventBind", result);
 				anElement.data(de.titus.core.EventBind.STATE.FINISHED, de.titus.core.EventBind.FINISHEDSTATE.READY);
-				return this;
+				return anElement;
 			}
 		};
-		
+
 		EventBind.EXPRESSIONRESOLVER = new de.titus.core.ExpressionResolver();
 		EventBind.STATE = {
 			FINISHED : "$$EventBind.FINISHED$$"
 		};
 		EventBind.FINISHEDSTATE = {
-		FAIL : "fail",
-		READY : "ready"
+		    FAIL : "fail",
+		    READY : "ready"
 		};
-		
+		EventBind.FUNCTIONS = {
+			"trigger-event" : function(anEvent) {
+				var element = $(anEvent.target);
+				element.trigger(element.attr("data-trigger-event"));
+			}
+		};
+
 		EventBind.$$__execute__$$ = function(anEvent) {
 			var element = $(this);
-			if (element.attr("event-prevent-default") != undefined)
+			var data = element.data("de.titus.core.EventBind");
+			if (data.preventDefault)
 				anEvent.preventDefault();
-			if (element.attr("event-stop-propagation") != undefined)
+			if (data.stopPropagation)
 				anEvent.stopPropagation();
-			
-			var action = element.attr("event-action");
-			action = de.titus.core.EventBind.EXPRESSIONRESOLVER.resolveExpression(action, anEvent.data, undefined);
-			if (typeof action === "function"){
+
+			var action = data.action;
+			if (!data.defaultAction)
+				action = EventBind.EXPRESSIONRESOLVER.resolveExpression(data.action, anEvent.data, undefined);
+
+			if (typeof action === "function") {
 				var args = Array.from(arguments);
-				if(args != undefined && args.length >= 1 && anEvent.data != undefined){
-					args.splice(1,0,anEvent.data);
-				}
+				if (args != undefined && args.length >= 1 && anEvent.data != undefined)
+					args.splice(1, 0, anEvent.data);
 				action.apply(action, args);
 			}
-			
+
 			return !anEvent.isDefaultPrevented();
 		};
 		de.titus.core.jquery.Components.asComponent("de.titus.core.EventBind", de.titus.core.EventBind);
-		
+
 		$(document).ready(function() {
 			var hasAutorun = $("[event-autorun]");
 			if (hasAutorun != undefined && hasAutorun.length != 0) {
 				$("[event-autorun]").de_titus_core_EventBind();
 				$("[event-autorun]").find("[event-type]").de_titus_core_EventBind();
-				
+
 				var observer = new MutationObserver(function(mutations) {
 					mutations.forEach(function(mutation) {
 						for (var i = 0; i < mutation.addedNodes.length; i++) {
@@ -79,15 +93,15 @@
 						}
 					});
 				});
-				
+
 				// configuration of the observer:
 				var config = {
-				attributes : false,
-				childList : true,
-				subtree : true,
-				characterData : false
+				    attributes : false,
+				    childList : true,
+				    subtree : true,
+				    characterData : false
 				};
-				
+
 				// pass in the target node, as well as the observer options
 				observer.observe(document.querySelector("[event-autorun]"), config);
 			}
